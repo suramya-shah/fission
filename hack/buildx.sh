96318378f4ff1ce6186a87acc2cf64ff7f91ecb7
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-07-27 11:29:08 +0530

    Update buildx.sh

--- hack/buildx.sh
+++ hack/buildx.sh
@@ -17,6 +17,6 @@ check_platform() {
 
 PLATFORMS=$(echo "$@" | tr ',' '\n')
 create_docker_builder
-for platform in "${PLATFORMS[@]}"; do
+for platform in "$@"; do
     check_platform $platform
 done
